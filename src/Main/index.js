d9fd1ebfcfcd4871b5fb5265f128582a52688dba
Rodrigo Fontes <rodrigo.cruz@ifs.edu.br>
2023-03-30 08:59:56 -0300

    Aula 1

---
+++ src/Main/index.js
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+import Header from '../components/Header';
+
+export default function Main() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
